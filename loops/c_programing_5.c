cc9e46f7ec775dfa06c755df88b38d509263bf8e
DEV <devkumarsah174@gmail.com>
2026-09-03 20:49:18 +0530

    Add files via upload

---
+++ loops/c_programing_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() {
+    int n;
+    printf("enter the value of n: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        //printf("%d\n", i);
+    {for(int j=1; j<=i; j++) {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
+}
