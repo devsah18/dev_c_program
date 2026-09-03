2d427d8f601bf34b9b3340ed7e4a74f4ca188bd8
DEV <devkumarsah174@gmail.com>
2026-09-03 20:59:02 +0530

    Update fabo.c
    Replaced prime number check with Fibonacci series calculation.

--- if_else/fabo.c
+++ if_else/fabo.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-void main()
-{
-    int n,isprime=1;
-    printf("enter the positive integer: ");
-    scanf("%d", &n);
-    if (n<=1)
+ void main()
+ {
+ int n,t1,t2,nextTerm;
+ printf("enter the number of terms: ");
+ scanf("%d", &n);   
+
+ if (n<=0)
+ {
+        printf("Error! Please enter a positive integer.");
+
+ 
+ }
+ else
+ { 
+    printf("Fibonacci Series: ");
+    t1 = 0;
+    t2 = 1; 
+ }
+
+    for (int i = 1; i <= n; ++i)
     {
-        isprime=0;
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
     }
-    else
-    {
-        for(int i=2; i<=n/2; i++)
-        {
-            if(n%i==0)
-            {
-                isprime=0;
-                break;
-            }
-        }
-    }
-    if(isprime==1)
-    {
-        printf("%d is a prime number.", n);
-    }
-    else
-    {
-        printf("%d is not a prime number.", n);
-    }
-    
-}
+    printf("\n");
+ }
+ 
